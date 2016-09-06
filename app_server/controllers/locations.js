4420d2137f6dc7b687ed1547de99410447fb1753
Omar Yafer Garcia Tecpa <omartecpa1@hotmail.com>
2016-09-06 18:36:43 -0500

    Chapter 07

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ var apiOptions = {
     server : "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = "https://loc8r-1456.herokuapp.com/";
+    apiOptions.server = "https://loc8r-1456.herokuapp.com";
 }
 
 
